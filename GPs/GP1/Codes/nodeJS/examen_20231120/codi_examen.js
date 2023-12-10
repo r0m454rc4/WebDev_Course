2b103376a7da2ba0e304c6889305d34067ad851f
romasarca <abyssextermination@gmail.com>
2023-12-10 01:21:41 +0000

    v.38

--- GPs/GP1/Codes/nodeJS/examen_20231120/codi_examen.js
+++ GPs/GP1/Codes/nodeJS/examen_20231120/codi_examen.js
@@ -1,6 +1,7 @@
 let http = require("http");
 let fs = require("fs");
-// let puppeteer = require("puppeteer");
+
+let llistaProductes = [];
 
 function iniciar() {
   function onRequest(req, res) {
@@ -18,12 +19,61 @@ function iniciar() {
           "Content-Type": "text/html; charset=utf-8",
         });
 
-        // console.log(sortida);
+        // productes.push(document.getElementById("seleccioProducte"));
+        console.log(sortida);
         res.write(sortida);
         res.end();
       });
+    } else if (reqUrl.pathname == "/compra") {
+      let consultaDemanada = reqUrl.searchParams; // This is because I'm sending the data using get, this is stored on the searchParams.
 
-      // document.getElementById("producte_seleccionat");
+      if (consultaDemanada.get("consulta") == "compra") {
+        console.log("COMPRA");
+
+        // producte is the selected option in the form.
+
+        let producte = consultaDemanada.get("producte");
+        let quantitatProductes = consultaDemanada.get("quantitatProductes");
+
+        for (let i = 0; i < quantitatProductes; i++) {
+          llistaProductes.push(producte);
+        }
+
+        console.log(
+          `Quantitat de productes seleccionats: ${quantitatProductes}`
+        );
+        console.log(`Producte seleccionat: ${producte}`);
+        console.log(`Llista de productes ${llistaProductes}`);
+
+        fs.readFile("index.html", function (err, sortida) {
+          res.writeHead(200, {
+            "Content-Type": "text/html; charset=utf-8",
+          });
+
+          // console.log(sortida);
+          res.write(sortida);
+          res.end();
+        });
+      } else if (consultaDemanada.get("consulta") == "tramita") {
+        console.log("TRAMITA");
+
+        fs.readFile("tramita.html", function (err, sortida) {
+          res.writeHead(200, {
+            "Content-Type": "text/html; charset=utf-8",
+          });
+
+          if (llistaProductes.length == 0) {
+            sortida += "No hi ha cap producte a la cistella.";
+          }
+
+          llistaProductes.forEach((producte) => {
+            sortida += `<li class="list-group-item">${producte}</li>`;
+          });
+
+          res.write(sortida);
+          res.end();
+        });
+      }
     } else {
       // If I don't ask for any of the pages that were before, I send "Not found".
 
