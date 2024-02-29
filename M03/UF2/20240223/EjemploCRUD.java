cf50901183acc33c621f603f160ad7802410fcb4
romasarca <w9tdango98osaw@proton.me>
2024-02-29 12:16:22 +0000

    v.77

--- M03/UF2/20240223/EjemploCRUD.java
+++ M03/UF2/20240223/EjemploCRUD.java
@@ -1,61 +1,49 @@
-import java.sql.*;
+import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
 
+// javac -cp .:mysql-connector-j-8.3.0.jar EjemploCRUD.java
+// java -cp .:mysql-connector-j-8.3.0.jar EjemploCRUD
+
 public class EjemploCRUD {
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String dni;
-		String nombre;
-		String telef;
-		String email;
 
 		try {
 			System.out.println("Introduzca el dni: ");
-			dni = sc.next();
+			String dni = sc.next();
 			System.out.println("Introduzca el nombre: ");
-			nombre = sc.next();
+			String nombre = sc.next();
 			System.out.println("Introduzca el telef: ");
-			telef = sc.next();
+			String telef = sc.next();
 			System.out.println("Introduzca email: ");
-			email = sc.next();
+			String email = sc.next();
 
-			// cargar el driver, para que Iona Marie.
-			// establecemos la conexion. parametros: donde esta la bd, usuario, contrase�a
-			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/bdclientes", "root", "");
-			// preparamos la consulta
-			Statement sentencia = conexion.createStatement();
-			// hacemos la consulta
-			// ResultSet resul = sentencia.executeQuery("insert into amigos values('"+dni+'
-			// '+nombre+"' '"+telef" )");
-			ResultSet resul = sentencia
-					.executeQuery(
-							"INSERT INTO amigos (dni, nombre, telef, email) \" + \"VALUES ('" + dni + "', '" + nombre + "', '" + telef
-									+ "', '" + email + "\");");
-			// recorremos el resultado para visualizar cada fila, con un bucle while
-			while (resul.next()) {
-				// Sacamos los resultados
-				System.out.println("Dni: " + resul.getString("dni"));
-				System.out.println("Nombre: " + resul.getString("nombre"));
-				System.out.println("Telef: " + resul.getString("telef"));
-				System.out.println("Email: " + resul.getString("email"));
+			// Establish connection
+			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/amigos", "root", "");
 
-				System.out.println("Tel�fono: " + resul.getString("telefono"));
-				System.out.println("Correo electr�nico: " + resul.getString("email"));
-				System.out.println("-----------------------------------------------\n");
-			} // fin del while
-				// cerramos resulSet
-			resul.close();
-			// cerramos Statement
-			sentencia.close();
-			// cerramos conexion
+			// Prepare statement
+			String query = "INSERT INTO amigos (dni, nombre, telef, email) VALUES (?, ?, ?, ?)";
+			PreparedStatement statement = conexion.prepareStatement(query);
+			statement.setString(1, dni);
+			statement.setString(2, nombre);
+			statement.setString(3, telef);
+			statement.setString(4, email);
+
+			// Execute the query.
+			int rowsInserted = statement.executeUpdate();
+			if (rowsInserted > 0) {
+				System.out.println("Una nova dada ha estat introduida!");
+			}
+
+			// Close resources.
+			statement.close();
 			conexion.close();
 			sc.close();
 		} catch (SQLException e) {
-			e.printStackTrace();
-		} // fin del try-catch
+			System.out.println("Error: " + e.getMessage());
+		}
 	}
 }
